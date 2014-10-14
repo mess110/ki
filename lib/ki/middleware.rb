e97d7eb25fbe36a1ef32cd5c8f21e32c61c5d97c
Cristian Mircea Messel <mess110@gmail.com>
2014-10-14 16:55:56 +0300

    set sass and coffee middleware content type header

--- lib/ki/middleware.rb
+++ lib/ki/middleware.rb
@@ -80,7 +80,7 @@ module Ki
         coffee_path = req.path.to_s[0...-3] + '.coffee'
         if !public_file_exists?(req) && format_of(req) == 'js' && public_file_exists?(coffee_path)
           js = CoffeeScript.compile(File.read(public_file_path(coffee_path)))
-          Rack::Response.new(js).finish
+          Rack::Response.new(js, 200, {"Content-Type" => "application/javascript"}).finish
         else
           @app.call env
         end
@@ -96,7 +96,7 @@ module Ki
         # if req ends with css and it does not exist, if a sass file exists instead
         if !public_file_exists?(req) && format_of(req) == 'css' && public_file_exists?(sass_path)
           eng = Sass::Engine.new(File.read(public_file_path(sass_path)), :syntax => :sass)
-          Rack::Response.new(eng.render).finish
+          Rack::Response.new(eng.render, 200, {"Content-Type" => "text/css"}).finish
         else
           @app.call env
         end
