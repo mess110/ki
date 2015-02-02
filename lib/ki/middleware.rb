3f171392f14e249d2030a8821c7bed1383077413
Cristian Mircea Messel <mess110@gmail.com>
2015-02-02 22:07:51 +0200

    server index.html if it exists

--- lib/ki/middleware.rb
+++ lib/ki/middleware.rb
@@ -29,9 +29,14 @@ module Ki
       def call env
         req = BaseRequest.new env
         if req.root?
-          resp = Rack::Response.new
-          resp.redirect('/index')
-          resp.finish
+          if public_file_exists? 'index.html'
+            env['PATH_INFO'] = '/index.html'
+            Rack::File.new(Ki::PUBLIC_PATH).call env
+          else
+            resp = Rack::Response.new
+            resp.redirect('/index')
+            resp.finish
+          end
         else
           env['CONTENT_TYPE'] = 'application/json' if format_of(req) == 'json'
           @app.call env
