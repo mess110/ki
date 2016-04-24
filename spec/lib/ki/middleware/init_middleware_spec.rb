0bec37fcf1c478206ffd42217b1d84e12ac6bc1f
Cristian Mircea Messel <mess110@gmail.com>
2016-04-25 00:50:43 +0300

    add init_middleware_spec

---
+++ spec/lib/ki/middleware/init_middleware_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Ki::Middleware::InitMiddleware do
+  let(:app) { {} }
+  let(:init) { Ki::Middleware::InitMiddleware.new app }
+  let(:req) { Ki::BaseRequest }
+
+  it 'redirects to /index if public/index.html does not exist' do
+    env = Rack::MockRequest.env_for('/', {'REQUEST_METHOD' => 'GET' })
+    resp = init.call env
+    expect(resp[0]).to eq 302 # redirect
+  end
+
+  it 'renders index.html if it exists' do
+    env = Rack::MockRequest.env_for('/', {'REQUEST_METHOD' => 'GET' })
+
+    Ki::Middleware::InitMiddleware.any_instance.stub(:public_file_exists?).and_return(true)
+    resp = init.call env
+    expect(resp[0]).to eq 404 # not found because index.html doesn't exist
+  end
+end
