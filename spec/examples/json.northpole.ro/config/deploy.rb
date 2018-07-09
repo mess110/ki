b0810e00ab9075b9af34ad20a4e3bddc1b09a61e
Cristian Mircea Messel <mess110@gmail.com>
2018-07-09 22:52:29 +0300

    tmp directory as linked

--- spec/examples/json.northpole.ro/config/deploy.rb
+++ spec/examples/json.northpole.ro/config/deploy.rb
@@ -27,6 +27,7 @@ set :deploy_to, '/home/kiki/json.northpole.ro'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{tmp}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
